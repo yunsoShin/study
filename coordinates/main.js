487e6d1a1a9be8681d737b15bdf8eb767bc31acf
yunsoo1 <shin4895@gmail.com>
2022-02-05 17:45:02 +0900

    JScoordinates

---
+++ coordinates/main.js
@@ -0,0 +1,21 @@
+const vertical = document.querySelector('.vertical');
+const horizontal = document.querySelector('.horizontal');
+const target = document.querySelector('.target');
+const tag = document.querySelector('.tag');
+
+document.addEventListener('mousemove' , (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
+    
+    console.log("move!");
+
+    console.log(`page: ${event.clientX},${event.clientY}`)
+    vertical.style.left = `${x}px`;
+    horizontal.style.top = `${y}px`;
+    target.style.top = `${y}px`;
+    target.style.left = `${x}px`;
+    tag.style.top = `${y}px`;
+    tag.style.left = `${x}px`;
+    
+
+})
