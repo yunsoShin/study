5c6de58167ce778319cb31ef6216679d0607f561
yunsoo1 <shin4895@gmail.com>
2022-02-08 21:32:23 +0900

    rabbits_js

---
+++ rabbits/main.js
@@ -0,0 +1,14 @@
+
+const rabbit=document.querySelector('.rabbit');
+const rabbits_button=document.querySelector('.rabbits_button');
+
+rabbits_button.addEventListener('click' , () => {
+  console.log("버튼을 눌렀습니다");
+
+})
+
+
+
+
+
+
