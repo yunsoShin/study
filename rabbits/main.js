fd44bc9ab76edbf80f4492fb8f182a59a43bbcd3
yunsoo1 <shin4895@gmail.com>
2022-02-08 21:48:29 +0900

    find rabbit finish

--- rabbits/main.js
+++ rabbits/main.js
@@ -1,10 +1,21 @@
 
 const rabbit=document.querySelector('.rabbit');
 const rabbits_button=document.querySelector('.rabbits_button');
+console.log(`page: ${rabbit.clientX},${rabbit.clientY}`)
+
+rabbit.addEventListener('click' , () => {
+  const rect = rabbit.getBoundingClientRect();
+  console.log(rect);
+  
+
+})
 
 rabbits_button.addEventListener('click' , () => {
+
+  const y = 100;
   console.log("버튼을 눌렀습니다");
 
+  scrollTo(0,392);
 })
 
 
