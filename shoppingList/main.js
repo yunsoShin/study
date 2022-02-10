69641be8768c702d0ead3bc76c32db6e85f137fb
yunsoo1 <shin4895@gmail.com>
2022-02-10 20:15:43 +0900

    addlist

--- shoppingList/main.js
+++ shoppingList/main.js
@@ -1,12 +1,20 @@
 const Add_listButton = document.querySelector('.Add_listButton');
 const Add_ListBox = document.querySelector('#Add_ListBox');
+const List = document.querySelector('.List');
 
 
 addEventListener('load' , ()=>{
-
-    Add_listButton.addEventListener('click' , ()=>{
-        console.log("클릭!");
-        console.log(Add_ListBox.value);
     
-    });
+    
+    
+    Add_listButton.addEventListener('click' , ()=>{
+        const temp = document.createElement('li');
+        temp.innerHTML = Add_ListBox.value;
+        List.appendChild(temp);
+        
+
+        
+
+    
+    })
 });
