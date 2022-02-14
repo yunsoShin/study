1ebb6a6ba47140ac34abcdf91108b34a749fa6b1
yunsoo1 <shin4895@gmail.com>
2022-02-14 23:58:54 +0900

    Update main.js

--- shoppingList/main.js
+++ shoppingList/main.js
@@ -4,8 +4,10 @@ const Add__Btn = document.querySelector('.Add__Btn');
 const Add__List = document.querySelector('.Add_List');
 
 addEventListener('load' , () => {
+    
     Add__Btn.addEventListener( 'click' , ()=> {
         console.log("click!");
+
     })
     
     
