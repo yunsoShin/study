09809cb1f8d3d4298aa2d759138c8f6c90c77d52
yunsoo1 <shin4895@gmail.com>
2022-02-14 23:47:46 +0900

    JS

--- shoppingList/main.js
+++ shoppingList/main.js
@@ -1,1 +1,14 @@
+const item__name = document.querySelector('.item__name');
+const delete__Btn = document.querySelector('.delete__Btn');
+const Add__Btn = document.querySelector('.Add__Btn');
+const Add__List = document.querySelector('.Add_List');
 
+addEventListener('load' , () => {
+    Add__Btn.addEventListener( 'click' , ()=> {
+        console.log("click!");
+    })
+    
+    
+    
+
+})
