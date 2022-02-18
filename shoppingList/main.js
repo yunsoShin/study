9986189272370d83af3a30bb287f8127a73effc1
yunsoo1 <shin4895@gmail.com>
2022-02-18 14:44:56 +0900

    todolist finish

--- shoppingList/main.js
+++ shoppingList/main.js
@@ -1,22 +1,46 @@
-const item__name = document.querySelector('.item__name');
 const delete__Btn = document.querySelector('.delete__Btn');
 const Add__Btn = document.querySelector('.Add__Btn');
-const Add__List = document.querySelector('.Add_List');
+const items = document.querySelector('.items');
+const Add__List = document.querySelector('#Add__List')
 
-addEventListener('load' , () => {
+function onAdd() {
+    const text = Add__List.value
+    const item = createItem(text);
+    items.appendChild(item);
+    console.log(text);
+    Add__List.value = '';
+    Add__List.focus();
+}
 
-
-
-
-
-
-
-    Add__Btn.addEventListener( 'click' , ()=> {
-        console.log("click!!");
-
-    })
-    
-    
-    
-
+Add__Btn.addEventListener('click' , ()=>{
+    onAdd();
 })
+
+function createItem(text){
+    const itemRow = document.createElement('li');
+    itemRow.setAttribute('class','item__row');
+    
+    const item = document.createElement('div')
+    item.setAttribute('class','item');
+
+    const name = document.createElement('span');
+    name.setAttribute('class','item__name');
+    name.innerText = text;
+
+    const DBtn = document.createElement('button');
+    DBtn.setAttribute('class','delete__Btn');
+    DBtn.innerHTML = '❌';
+    DBtn.addEventListener('click' , ()=>{
+        items.removeChild(itemRow);
+    })
+    const itemDivider = document.createElement('div');
+    itemDivider.setAttribute('class','item__divider');
+
+    item.appendChild(name);
+    item.appendChild(DBtn);
+
+    itemRow.appendChild(item);
+    itemRow.appendChild(itemDivider);
+
+    return itemRow;
+}
