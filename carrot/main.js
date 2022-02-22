9a97f6c1b6fcd3da0a011ae81e8da12325cf9d9b
yunsoo1 <shin4895@gmail.com>
2022-02-22 17:27:35 +0900

    HTMLmarkup

---
+++ carrot/main.js
@@ -0,0 +1,7 @@
+<div class="suc">
+                <button class="suc_Btn">
+                    ▶︎
+                </button>
+                <span>you win!</span>
+            </div>
+            
