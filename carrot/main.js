2ca0fd34536648918d9d4b60ae52eb566ec1bcc8
yunsoo1 <shin4895@gmail.com>
2022-03-07 20:25:56 +0900

    make IMG

--- carrot/main.js
+++ carrot/main.js
@@ -2,16 +2,36 @@ const game__field = document.querySelector('.game__field');
 const gmae_Btn = document.querySelector('.gmae_Btn');
 const game__timer = document.querySelector('.game__timer');
 const popup__refresh = document.querySelector('.popup__refresh');
+const bug = document.querySelector('.bug');
+const carrot = document.querySelector('.carrot');
 
-function init(){
-    
-    const bug = document.createElement('img');
-    bug.setAttribute('src','img/bug.png');
 
-    const carrot = document.createElement('img');
-    carrot.setAttribute('src','img/carrot.png');
+
+
+const fieldRect = game__field.getBoundingClientRect();
+
+function initGame() {
+
+
+    console.log(fieldRect);
+
+    const carrotSRC =document.createElement('img');
+    carrotSRC.setAttribute('src','img/carrot.png');
+    carrotSRC.setAttribute('class','carrot');
+    //carrotSRC.innerHTML = 'img/carrot.png';//
+
+    const bugSRC = document.createElement('img');
+    bugSRC.setAttribute('src','img/bug.png');
+    bugSRC.setAttribute('class','bug');
+
+
+    game__field.appendChild(carrotSRC);
+    game__field.appendChild(bugSRC);
     
+    const bugRECT = bugSRC.getBoundingClientRect();
+    console.log(bugRECT);
+
 
 }
-init();
 
+initGame();
