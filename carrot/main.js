815149e6ef8e6405f5cc364386adb26cd2d88dc8
yunsoo1 <shin4895@gmail.com>
2022-03-07 19:20:45 +0900

    mac to win

--- carrot/main.js
+++ carrot/main.js
@@ -1,7 +1,16 @@
-<div class="suc">
-                <button class="suc_Btn">
-                    ▶︎
-                </button>
-                <span>you win!</span>
-            </div>
-            
+const game__field = document.querySelector('.game__field');
+const gmae_Btn = document.querySelector('.gmae_Btn');
+const game__timer = document.querySelector('.game__timer');
+const popup__refresh = document.querySelector('.popup__refresh');
+
+function init(){
+    
+    const bug = document.createElement('img');
+    bug.setAttribute('src','img/bug.png');
+
+    const carrot = document.createElement('img');
+    carrot.setAttribute('src','img/carrot.png');
+    
+
+}
+init();
