c0dda8ffdca4884b92be657418365b1f0009f9f0
yunsoo1 <shin4895@gmail.com>
2022-03-07 20:51:58 +0900

    random
    랜덤하게 이미지 5개 생성 후 필드에 보여주기

--- carrot/main.js
+++ carrot/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const game__field = document.querySelector('.game__field');
 const gmae_Btn = document.querySelector('.gmae_Btn');
 const game__timer = document.querySelector('.game__timer');
@@ -10,11 +12,14 @@ const carrot = document.querySelector('.carrot');
 
 const fieldRect = game__field.getBoundingClientRect();
 
+
 function initGame() {
 
 
     console.log(fieldRect);
-
+    addItem('carrot' , 5 , 'img/carrot.png');
+    addItem('bug' , 5 , 'img/bug.png');
+    /*
     const carrotSRC =document.createElement('img');
     carrotSRC.setAttribute('src','img/carrot.png');
     carrotSRC.setAttribute('class','carrot');
@@ -29,9 +34,35 @@ function initGame() {
     game__field.appendChild(bugSRC);
     
     const bugRECT = bugSRC.getBoundingClientRect();
-    console.log(bugRECT);
+    */
+    
 
 
+}
+function addItem(className, count ,imgPath){
+    const x1 = 0;
+    const y1 = 0;
+    const x2 = fieldRect.width;
+    const y2 = fieldRect.height;
+    
+    for(let i=0; i<count; i++){
+        const item =document.createElement('img');
+        item.setAttribute('src',imgPath);
+        item.setAttribute('class',className);
+        item.style.position = 'absolute';
+        const x=randomNumber(x1,x2);
+        const y=randomNumber(y1,y2);
+        item.style.left=`${x}px`;
+        item.style.top=`${y}px`;
+        game__field.appendChild(item);
+
+        
+
+    }
+}
+
+function randomNumber(min,max){
+    return Math.random()*(max - min)+min;
 }
 
 initGame();
