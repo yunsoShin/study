25d6073eb29603ed06b6718feaf2bdbf0fcaff7f
yunsoo1 <shin4895@gmail.com>
2022-03-08 18:37:11 +0900

    timer

--- carrot/main.js
+++ carrot/main.js
@@ -1,49 +1,25 @@
 'use strict';
 
 const game__field = document.querySelector('.game__field');
-const gmae_Btn = document.querySelector('.gmae_Btn');
+const game_Btn = document.querySelector('.game_Btn');
 const game__timer = document.querySelector('.game__timer');
 const popup__refresh = document.querySelector('.popup__refresh');
 const bug = document.querySelector('.bug');
 const carrot = document.querySelector('.carrot');
-
-
-
-
+const score = document.querySelector('.game__score');
 const fieldRect = game__field.getBoundingClientRect();
 
 
 function initGame() {
-
-
     console.log(fieldRect);
     addItem('carrot' , 5 , 'img/carrot.png');
     addItem('bug' , 5 , 'img/bug.png');
-    /*
-    const carrotSRC =document.createElement('img');
-    carrotSRC.setAttribute('src','img/carrot.png');
-    carrotSRC.setAttribute('class','carrot');
-    //carrotSRC.innerHTML = 'img/carrot.png';//
-
-    const bugSRC = document.createElement('img');
-    bugSRC.setAttribute('src','img/bug.png');
-    bugSRC.setAttribute('class','bug');
-
-
-    game__field.appendChild(carrotSRC);
-    game__field.appendChild(bugSRC);
-    
-    const bugRECT = bugSRC.getBoundingClientRect();
-    */
-    
-
-
 }
 function addItem(className, count ,imgPath){
     const x1 = 0;
     const y1 = 0;
-    const x2 = fieldRect.width;
-    const y2 = fieldRect.height;
+    const x2 = fieldRect.width - 50;
+    const y2 = fieldRect.height - 50;
     
     for(let i=0; i<count; i++){
         const item =document.createElement('img');
@@ -55,14 +31,33 @@ function addItem(className, count ,imgPath){
         item.style.left=`${x}px`;
         item.style.top=`${y}px`;
         game__field.appendChild(item);
-
-        
-
     }
+    score.innerHTML = count
 }
 
 function randomNumber(min,max){
-    return Math.random()*(max - min)+min;
+    return Math.random()*(max - min)+min
 }
 
-initGame();
+game_Btn.addEventListener('click',()=>{
+    
+    game_Btn.setAttribute('class','game__stop');
+    initGame();
+    let time = 5;
+    let timer = setInterval(function(){
+        
+            game__timer.innerHTML =`0:${time}`;
+            time--
+            console.log(time);
+            if(time<0){
+                replay();
+                clearInterval(timer);
+            }
+        
+    }, 1000)
+
+})
+
+function replay(){
+    
+};
