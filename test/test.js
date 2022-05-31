272b2d4c840ed0a9f9bc88df5906729067eb2bc2
yunsoo1 <shin4895@gmail.com>
2022-05-31 10:58:40 +0900

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,12 +1,20 @@
-console.log('js load');
 
-const obj = {
-    name: 'yunsoo',
+const PI = 3.14159265;
+const notQuitePi = parseFloat('3.14') // 숫자 3.14 
+const evenLessPi = parseInt('3') // 정수 3
 
+console.log(notQuitePi,evenLessPi)
 
-    
-    age: '20'
-};
+let YU = 'yunjjang'.substring(0, 2.5) // 문자열 'yu'
 
+console.log(YU);
 
-obj && console.log('true' , obj);
+let IndexYu = 'yunjjang'[2.5] // undefined
+
+console.log(IndexYu);
+
+let gu = 9;
+let Name = 'yunJJang'+gu
+console.log(Name);
+let TypeName = typeof(Name);
+console.log(TypeName);
