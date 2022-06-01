7fec4659ab0409da17b7c3dd4895a82f4dfd754b
yunsoo1 <shin4895@gmail.com>
2022-06-01 09:59:02 +0900

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,9 +1,7 @@
 function add(x,y){
-    x+y
-    
-
+    console.log(x+y)
 }
 let AGE = 2
-let yun = {name:'yunsoo shin' , AGE: [add(AGE,AGE)]}
+let yun = {name:'yunsoo shin',AGE}
 console.log(yun)
 console.log(add(3,2))
