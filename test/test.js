5689eb8f20d15aedde7a722845862804f94c4e1d
yunsoo1 <shin4895@gmail.com>
2022-06-01 10:06:45 +0900

    Update test.js

--- test/test.js
+++ test/test.js
@@ -10,3 +10,11 @@ const obj = {
 
 
 obj && console.log('true' , obj);
+
+function add(x,y){
+    console.log(x+y)
+}
+let AGE = 2
+let yun = {name:'yunsoo shin',AGE}
+console.log(yun)
+console.log(add(3,2));
