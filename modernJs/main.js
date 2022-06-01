6c042c4ef77569325dfd8634bdf5e245d6d774da
yunsoo1 <shin4895@gmail.com>
2022-06-01 10:08:36 +0900

    mJS

--- modernJs/main.js
+++ modernJs/main.js
@@ -4,3 +4,11 @@ let a= 6;
 window.alert(a*b);
 console.log(typeof(a));
 console.log(typeof(null));
+
+function add(x,y){
+    console.log(x+y)
+}
+let AGE = 2
+let yun = {name:'yunsoo shin',AGE}
+console.log(yun)
+console.log(add(3,2));
