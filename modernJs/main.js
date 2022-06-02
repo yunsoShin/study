9c1dcf145b29a9380542a06f8d33a1a1667f0027
yunsoo1 <shin4895@gmail.com>
2022-06-02 14:02:58 +0900

    js

--- modernJs/main.js
+++ modernJs/main.js
@@ -1,14 +1,3 @@
-
-let b = 7;
-let a= 6;
-window.alert(a*b);
-console.log(typeof(a));
-console.log(typeof(null));
-
-function add(x,y){
-    console.log(x+y)
-}
-let AGE = 2
-let yun = {name:'yunsoo shin',AGE}
-console.log(yun)
-console.log(add(3,2));
+const number = [1,2,3,'num']
+console.log(number['1'])
+console.log(number.length)
