f9f0a70d2957caab81f4232bf9b618f66d121827
yunsoo1 <shin4895@gmail.com>
2022-06-03 21:39:13 +0900

    Update main.js

--- modernJs/main.js
+++ modernJs/main.js
@@ -1,8 +1,7 @@
-/*const number = [
 
+/*const number = [
     [1,2,3,4],
     [5,6,7,8]
-
 ]
 console.log(number[1][3])
 console.log(number.length)
@@ -16,15 +15,37 @@ console.log(second)
 */
 
 //비구조화
-let pair = [1,2,3,4,5]
+//let pair = [1,2,3,4,5]
 //let first = pair[0]
 //let second = pair[1]
 
 //let[first,[second,third]]=[0,[1,2]]
 //console.log(first,second,third)
 
-/*let[first,second]=[0,1,2]
+/*let[first,second]=[0,1]
+console.log(first,second)
+let[first,second]=[0,1]
+
 console.log(first,second)*/
 
-let [first,second,third]=[0]
-console.log(first,second,third)
+
+
+
+/*let [first,second,third]=[0]
+console.log(first,second,third)*/
+
+/*let number=[3,2,1];
+let yun={age:22};
+[number[0],yun.age]=[1,24];
+
+console.log(number[0],yun.age);*/
+
+
+/*let {name,age} = yun
+let {name:name,age:age}=yun;*/
+
+//let name = yun.name;
+//let age = yun.age;
+
+
+
