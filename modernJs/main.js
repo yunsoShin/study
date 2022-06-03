0aca1c882420a73846f781831da03cc252437be9
yunsoo1 <shin4895@gmail.com>
2022-06-03 18:19:15 +0900

    Update main.js

--- modernJs/main.js
+++ modernJs/main.js
@@ -1,5 +1,13 @@
-const number = [1,2,3,'num']
-console.log(number['1'])
+const number = [
+    [1,2,3,4],
+    [5,6,7,8]
+
+
+]
+
+
+
+
+console.log(number[0],number[1])
 console.log(number.length)
 
-//미들레
