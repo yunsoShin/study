d821265929045ff6a3f11404cfd6c58cccb2df7f
yunsoo1 <shin4895@gmail.com>
2022-06-03 20:55:30 +0900

    Update main.js

--- modernJs/main.js
+++ modernJs/main.js
@@ -1,7 +1,26 @@
-const number = [
+/*const number = [
+
     [1,2,3,4],
     [5,6,7,8]
+
 ]
 console.log(number[1][3])
 console.log(number.length)
+
+let pair = [1,2,3,4,5]
+let first = pair[0]
+let second = pair[1]
+
+console.log(first)
+console.log(second)
+*/
+
+//비구조화
+let pair = [1,2,3,4,5]
+let first = pair[0]
+let second = pair[1]
+
+console.log(first)
+console.log(second)
+
 
