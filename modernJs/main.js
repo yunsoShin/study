91bc79e8c6d188072f214b816147120d6100eb0a
yunsoo1 <shin4895@gmail.com>
2022-06-03 21:04:19 +0900

    Update main.js

--- modernJs/main.js
+++ modernJs/main.js
@@ -17,10 +17,14 @@ console.log(second)
 
 //비구조화
 let pair = [1,2,3,4,5]
-let first = pair[0]
-let second = pair[1]
+//let first = pair[0]
+//let second = pair[1]
 
-console.log(first)
-console.log(second)
+//let[first,[second,third]]=[0,[1,2]]
+//console.log(first,second,third)
 
+/*let[first,second]=[0,1,2]
+console.log(first,second)*/
 
+let [first,second,third]=[0]
+console.log(first,second,third)
