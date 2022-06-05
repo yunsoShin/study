e77bb25f067a52c20657d4bda4c0bcdf4648ce50
yunsoo1 <shin4895@gmail.com>
2022-06-05 20:14:32 +0900

    destructuring assignment
    배열의 비구조화,나머지,객체의 비구조화

--- modernJs/main.js
+++ modernJs/main.js
@@ -49,3 +49,16 @@ let {name:name,age:age}=yun;*/
 
 
 
+/*let pat = { name: 'Pat', birthday: { day: 14, month: 3, year: 2000 } };
+
+let { birthday: { year: patsBirthYear } } = pat;
+
+//let patsBirthYear = pat.birthday.year과 의미가 같다
+console.log(patsBirthYear);
+*/
+
+let numbers = [1, 7, 2, 9]
+let [first, second, ...others] = numbers
+
+console.log(numbers);
+console.log(others);
